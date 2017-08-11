97b8cb810632ebe27d8699d6344dcb5255c0b2eb
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-08-11 00:12:54 -0500

    started on component testing

--- src/components/hello/index-spec.jsx
+++ src/components/hello/index-spec.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
+import { expect }from 'chai'; 
 
 import ConnectedComponent from './';
+import { ActionTypes } from '../../actions/action-types';
 import configureStore from '../../store/configureStore';
 
 describe('Hello Connected Component', () => {
@@ -12,13 +14,42 @@ describe('Hello Connected Component', () => {
     store = configureStore();
   });
 
-  it('shows loading when screen is loaded', () => {
+  it('shows loading when screen is loading', () => {
     const testObject = mount(
       <Provider store={store} >
         <ConnectedComponent />
       </Provider>,
     );
+
+    const initialStore = store.getState(ActionTypes.GREETING_REQUEST).greeting;
+    expect(initialStore).to.eql({greetingText: 'loading...'});
+
+    const p = testObject.find('p');
+    expect(p.length).to.equal(1);
+    expect(p.text()).to.eql('loading...');
   });
-  it('shows greeting after successfully fetched');
+
+  function wait(delay) {
+    return new Promise(resolve => setTimeout(() => resolve(), delay));
+  }
+
+  it('shows greeting after successfully fetched', () => {
+    const testObject = mount(
+      <Provider store={store} >
+        <ConnectedComponent />
+      </Provider>,
+    );
+
+    return wait(1000).then(() => {
+      const state = store.getState(ActionTypes.GREETING_SENT).greeting;
+      expect(state).to.eql({greetingText: 'Hello world!'});
+
+      const p = testObject.find('p');
+      expect(p.length).to.equal(1);
+      expect(p.text()).to.eql('Hello world!');
+    });
+
+  });
+
   it('shows error when greeting fetch fails');
 });
