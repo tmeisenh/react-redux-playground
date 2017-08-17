598bc7e6cb1d8bb712aaa27c75534ef521f26790
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-08-16 19:00:21 -0500

    add delay back in

--- src/services/greeting.js
+++ src/services/greeting.js
@@ -1,5 +1,3 @@
-
-// const greeting = () => new Promise((resolve, reject) => setTimeout(() => resolve('Hello world!'), 1000));
-const greeting = () => new Promise((resolve, reject) => resolve('Hello world!'));
+const greeting = () => new Promise((resolve, reject) => setTimeout(() => resolve('Hello world!'), 1000));
 
 export const api = { greeting };
