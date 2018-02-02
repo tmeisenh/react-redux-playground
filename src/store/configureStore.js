60ddd298edba54a111b0e9e176258c0d4506f169
Travis Meisenheimer <travis@indexoutofbounds.com>
2018-02-01 18:52:34 -0600

    Only add the redux-logger if we are in the development envrionment

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,11 +7,15 @@ import sagas from '../sagas';
 export default () => {
   const sagaMiddleware = createSagaMiddleware();
 
+  const middleWares = [sagaMiddleware];
+  if (process.env.NODE_ENV === 'development') {
+    middleWares.push(logger);
+  }
 
   const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(logger, sagaMiddleware),
+    applyMiddleware(...middleWares),
   );
 
   sagaMiddleware.run(sagas);
